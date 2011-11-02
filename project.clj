46b388803da9c914c9aed9aa4d1f4f22f874d0b0
sanakan <christoph.j.wagner@gmail.com>
2011-11-02 19:51:52 +0100

    added processing sketch for verification of algorithms

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject mathematics "1.0.0-SNAPSHOT"
-  :description "Implementation of some mathematical functions."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+            :description "Implementation of some mathematical functions."
+            :dependencies [[org.clojure/clojure "1.2.0"]
+                           [org.clojure/clojure-contrib "1.2.0"]
+                           [org.clojars.automata/rosado.processing "1.1.0"]]
+            :main sanakan.mathematics.core)
