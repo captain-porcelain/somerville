f7bbd2c9d69a15637d8bf12a89098df8afa98b5b
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2014-12-18 20:29:13 +0100

    conversion to lab color space

--- src/sanakan/mathematics/color/color.clj
+++ src/sanakan/mathematics/color/color.clj
@@ -1,5 +1,4 @@
-(ns sanakan.mathematics.color.color
-  (:import java.util.Random))
+(ns sanakan.mathematics.color.color)
 
 (defrecord ColorRGBA [r g b a])
 (defrecord ColorLab [l a b])
@@ -23,13 +22,42 @@
 
 (def white-d50 (ColorXYZ. 0.964221 1.0 0.825211))
 
+(defn- xyzconv1
+  [c]
+  (if (<= c 0.040) (/ c 12) (java.lang.Math/pow (/ (+ c 0.055) 1.055) 2.4)))
+
+(defn- xyzconv2
+  [c]
+  (let [eps (/ 216.0 24389.0)
+        k (/ 24389.0 27.0)]
+    (if (> c eps) (java.lang.Math/pow c (/ 1 3)) (/ (+ (* k c) 16.0) 116.0))))
+
 (defn xyz
-  "Convert RGBA to XYZ."
+  "Convert RGBA to XYZ. Found at http://stackoverflow.com/questions/4593469/java-how-to-convert-rgb-color-to-cie-lab"
   [rgb]
-  (ColorXYZ. (/ (:r rgb) 255.0) (/ (:g rgb) 255.0) (/ (:b rgb) 255.0)))
+  (let [r (xyzconv1 (/ (:r rgb) 255.0))
+        g (xyzconv1 (/ (:g rgb) 255.0))
+        b (xyzconv1 (/ (:b rgb) 255.0))
+        Xr 0.964221
+        Yr 1.0
+        Zr 0.825211
+        X (+ (* 0.436052025 r) (* 0.385081593 g) (+ 0.143087414 b))
+        Y (+ (* 0.222491598 r) (* 0.71688606 g) (+ 0.060621486 b))
+        Z (+ (* 0.013929122 r) (* 0.097097002 g) (+ 0.71418547 b))
+        xr (/ X Xr)
+        yr (/ Y Yr)
+        zr (/ Z Zr)]
+    (ColorXYZ. xr yr zr)))
 
 (defn lab
   "Convert a rgb color to Lab colorspace."
   [rgb]
-  )
+  (let [cxyz (xyz rgb)
+        fx (xyzconv2 (:x cxyz))
+        fy (xyzconv2 (:y cxyz))
+        fz (xyzconv2 (:z cxyz))
+        Ls (- (* 116 fy) 16)
+        as (* 500 (- fx fy))
+        bs (* 200 (- fy fz))]
+    (ColorLab. (int (+ (* 2.55 Ls) 0.5)) (int (+ as 0.5)) (int (+ bs 0.5)))))
 
