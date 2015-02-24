0222e78f31e18ae0adfaf8a723254a68182a6e7e
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2015-02-24 20:01:27 +0100

    add first version of flood fill

---
+++ src/sanakan/mathematics/flood_fill.clj
@@ -0,0 +1,49 @@
+;; Provides a simple algorithm to flood fill an area
+(ns sanakan.mathematics.flood-fill)
+
+(defn neighbour?
+  "Are two points neighbours?"
+  [p1 p2]
+  (let [dx (- (:x p2) (:x p1))
+        dy (- (:y p2) (:y p1))
+        dx (if (< dx 0) (* -1 dx) dx)
+        dy (if (< dy 0) (* -1 dy) dy)]
+    (or
+      (and (= 1 dx) (= 1 dy))
+      (and (= 1 dx) (= 0 dy))
+      (and (= 0 dx) (= 1 dy)))))
+
+(defn neighbours
+  "Filter list of points to those that are neighbours of the given point."
+  [points point]
+  (filter #(neighbour? point %) points))
+
+(defn add-value
+  "Update all points with their value."
+  [points value-fn]
+  (map #(assoc % :value (value-fn %)) points))
+
+(defn test-neighbours
+  "Test all neighbours and return those that should be added."
+  [point points decider-fn]
+  (let [n (neighbours points point)]
+    (filter #(decider-fn point %) n)))
+
+(defn fill
+  "Start with the first point in the list of points and take as long as
+  the decider function returns true for the results of the value function
+  of the current point and the next one.
+  The value-fn should take a Point and return some value.
+  The decider-fn should take a point with a value and a neighbour with
+  value and return true/false accordingly if the neighbour is reachable."
+  [seed points value-fn decider-fn]
+  (let [testpoints (add-value points value-fn)] ;; add values to all points
+    (loop [newpoints (list (assoc seed :value (value-fn seed))) ;; put seed into candidates list
+           accepted '()]
+      (if (= 0 (count newpoints))
+        accepted
+        (let [p (first newpoints) ;; take seed
+              n (test-neighbours p testpoints decider-fn) ;; and get fitting neighbours
+              np (concat (rest newpoints) (remove (set (concat newpoints accepted)) n)) ;; filter neighbours to new ones
+              ap (conj accepted p)]
+          (recur np ap))))))
--- test/sanakan/mathematics/color/color_test.clj
+++ test/sanakan/mathematics/color/color_test.clj
@@ -1,6 +1,7 @@
 (ns sanakan.mathematics.color.color-test
   (:require
-    [sanakan.mathematics.color.color :as c])
+    [sanakan.mathematics.color.color :as c]
+    [sanakan.mathematics.geometry.commons :as gc])
   (:use midje.sweet))
 
 (def c1 (c/rgba -16777216))
@@ -12,3 +13,12 @@
 (fact (:r c2) => 232)
 (fact (:g c2) => 82)
 (fact (:b c2) => 228)
+
+(def c1 (c/rgba 0 0 0))
+(def c2 (c/rgba 1 1 1))
+(def c3 (c/rgba 2 2 2))
+(def c4 (c/rgba 10 10 10))
+(fact (c/cie76 c1 c1) => 0.0)
+(fact (c/cie76 c1 c2) => 1.0)
+(fact (c/cie76 c1 c3) => 1.0)
+(fact (gc/close-to (c/cie76 c1 c4) 3.741657) => true)
