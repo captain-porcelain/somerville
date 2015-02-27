600db5c9fba5c9f97ff511098816c69a478ac051
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2015-02-27 18:31:02 +0100

    performance testing

--- src/sanakan/mathematics/flood_fill.clj
+++ src/sanakan/mathematics/flood_fill.clj
@@ -1,5 +1,7 @@
 ;; Provides a simple algorithm to flood fill an area
-(ns sanakan.mathematics.flood-fill)
+(ns sanakan.mathematics.flood-fill
+  (:require
+    [sanakan.mathematics.geometry.point :as p]))
 
 (defn neighbour?
   "Are two points neighbours?"
@@ -18,10 +20,18 @@
   [points point]
   (filter #(neighbour? point %) points))
 
-(defn add-value
-  "Update all points with their value."
-  [points value-fn]
-  (map #(assoc % :value (value-fn %)) points))
+(defn neighbours2
+  "Filter list of points to those that are neighbours of the given point."
+  [point]
+  (list
+    (p/point (- (:x point) 1)  (- (:y point) 1))
+    (p/point (- (:x point) 1)  (:y point))
+    (p/point (- (:x point) 1)  (+ (:y point) 1))
+    (p/point (:x point)  (- (:y point) 1))
+    (p/point (:x point)  (+ (:y point) 1))
+    (p/point (+ (:x point) 1)  (- (:y point) 1))
+    (p/point (+ (:x point) 1)  (:y point))
+    (p/point (+ (:x point) 1)  (+ (:y point) 1))))
 
 (defn test-neighbours
   "Test all neighbours and return those that should be added."
@@ -29,36 +39,88 @@
   (let [n (neighbours points point)]
     (filter #(decider-fn point %) n)))
 
+(defn in-bounds?
+  "Check if a pixel is inside an image."
+  [p x1 y1 x2 y2]
+  (and
+    (<= (:x p) x2)
+    (>= (:x p) x1)
+    (<= (:y p) y2)
+    (>= (:y p) y1)))
+
+(defn test-neighbours2
+  "Test all neighbours and return those that should be added."
+  [point decider-fn x1 y1 x2 y2]
+  (let [n (filter #(in-bounds? % x1 y1 x2 y2) (neighbours2 point))]
+    (filter #(decider-fn point %) n)))
+
 (defn fill
   "Start with the first point in the list of points and take as long as
   the decider function returns true for the results of the value function
-  of the current point and the next one.
-  The value-fn should take a Point and return some value.
-  The decider-fn should take a point with a value and a neighbour with
-  value and return true/false accordingly if the neighbour is reachable."
-  [seed points value-fn decider-fn]
-  (let [testpoints (add-value points value-fn)] ;; add values to all points
-    (loop [newpoints (list (assoc seed :value (value-fn seed))) ;; put seed into candidates list
-           accepted '()]
-      (if (= 0 (count newpoints))
-        accepted
-        (let [p (first newpoints) ;; take seed
-              n (test-neighbours p testpoints decider-fn) ;; and get fitting neighbours
-              np (concat (rest newpoints) (remove (set (concat newpoints accepted)) n)) ;; filter neighbours to new ones
-              ap (conj accepted p)]
-          (recur np ap))))))
+  of the current point and the next one."
+  [seed points decider-fn]
+  (loop [newpoints (list seed) ;; put seed into candidates list
+         i 0
+         accepted '()]
+    (if (= 0 (count newpoints))
+      accepted
+      (let [p (first newpoints) ;; take seed
+            n (test-neighbours p points decider-fn) ;; and get fitting neighbours
+            np (concat (rest newpoints) (remove (set (concat newpoints accepted)) n)) ;; filter neighbours to new ones
+            ap (conj accepted p)
+            ;tmp (dorun (println (str "run " i ": " (count np) " - " (count ap))))
+            ]
+        (recur np (+ i 1) ap)))))
+
+(defn fill2
+  "Start with the first point in the list of points and take as long as
+  the decider function returns true for the results of the value function
+  of the current point and the next one."
+  [seed points decider-fn x1 y1 x2 y2]
+  (loop [newpoints (list seed) ;; put seed into candidates list
+         i 0
+         accepted '()]
+    (if (= 0 (count newpoints))
+      accepted
+      (let [p (first newpoints) ;; take seed
+            n (test-neighbours2 p decider-fn x1 y1 x2 y2) ;; and get fitting neighbours
+            np (concat (rest newpoints) (remove (set (concat newpoints accepted)) n)) ;; filter neighbours to new ones
+            ap (conj accepted p)
+            ;tmp (dorun (println (str "run " i ": " (count np) " - " (count ap))))
+            ]
+        (recur np (+ i 1) ap)))))
 
 (defn partition
-  "Use flood fill to partition a space and return a list of partitions with their points."
-  [points value-fn decider-fn]
-  (loop [remaining (add-value points value-fn)
+  "Use flood fill to partition a space and return a list of partitions with their points.
+  The decider-fn should take a point and a neighbour and return true/false accordingly if
+  the neighbour is reachable."
+  [points decider-fn]
+  (loop [remaining points
          i 0
          partitions '()]
     (if (= 0 (count remaining))
       partitions
       (let [seed (first remaining)
             testpoints (rest remaining)
-            part (fill seed testpoints value-fn decider-fn)
+            part (fill seed testpoints decider-fn)
+            restpoints (remove (set part) testpoints)
+            ;tmp (dorun (println (str "run " i ": " (count part) " - " (count restpoints))))
+            ]
+        (recur restpoints (+ i 1) (conj partitions part))))))
+
+(defn partition2
+  "Use flood fill to partition a space and return a list of partitions with their points.
+  The decider-fn should take a point and a neighbour and return true/false accordingly if
+  the neighbour is reachable."
+  [points decider-fn x1 y1 x2 y2]
+  (loop [remaining points
+         i 0
+         partitions '()]
+    (if (= 0 (count remaining))
+      partitions
+      (let [seed (first remaining)
+            testpoints (rest remaining)
+            part (fill2 seed testpoints decider-fn x1 y1 x2 y2)
             restpoints (remove (set part) testpoints)
             ;tmp (dorun (println (str "run " i ": " (count part) " - " (count restpoints))))
             ]
--- test/sanakan/mathematics/flood_fill_test.clj
+++ test/sanakan/mathematics/flood_fill_test.clj
@@ -24,39 +24,166 @@
   (and (< (:x p) 0) (< (:y p) 0)))
 
 (defn negative-decider-fn
-  [v nv]
-  (:value nv))
+  [p1 p2]
+  (and (< (:x p2) 0) (< (:y p2) 0)))
 
 (defn blocked-value-fn
   [p]
   (if (= (:x p) 2) 100 (:x p)))
 
 (defn blocked-decider-fn
-  [v nv]
-  (and (< -2 (- (:value v) (:value nv))) (> 2 (- (:value v) (:value nv)))))
+  [p1 p2]
+  (let [vfn (fn [p] (if (= (:x p) 2) 100 (:x p)))
+        v1 (vfn p1)
+        v2 (vfn p2)]
+    (and (< -2 (- v1 v2)) (> 2 (- v1 v2)))))
 
-(fact (blocked-decider-fn {:value 1} {:value 2}) => true)
-(fact (blocked-decider-fn {:value 100} {:value 2}) => false)
-(fact (blocked-decider-fn {:value 2} {:value 100}) => false)
-
-(def neg-v-points (ff/add-value points negative-value-fn))
+(fact (blocked-decider-fn (p/point 1 0) (p/point 0 0)) => true)
+(fact (blocked-decider-fn (p/point 1 0) (p/point 2 0)) => false)
+(fact (blocked-decider-fn (p/point 2 0) (p/point 3 0)) => false)
 
 (def p1 (p/point -1 -1))
-(def neg-v-p1 (assoc p1 :value (negative-value-fn p1)))
-(fact (count (ff/test-neighbours neg-v-p1 neg-v-points negative-decider-fn)) => 3)
+(fact (count (ff/test-neighbours p1 points negative-decider-fn)) => 3)
 
 (def p2 (p/point 0 0))
-(def neg-v-p2 (assoc p2 :value (negative-value-fn p2)))
-(fact (count (ff/test-neighbours neg-v-p2 neg-v-points negative-decider-fn)) => 1)
+(fact (count (ff/test-neighbours p2 points negative-decider-fn)) => 1)
 
 (def p3 (p/point -2 -2))
-(def neg-v-p3 (assoc p3 :value (negative-value-fn p3)))
-(fact (count (ff/test-neighbours neg-v-p3 neg-v-points negative-decider-fn)) => 8)
+(fact (count (ff/test-neighbours p3 points negative-decider-fn)) => 8)
 
-(fact (count (ff/fill (p/point -1 -1) points negative-value-fn negative-decider-fn)) => 25)
+(fact (ff/in-bounds? p1 -10 -10 10 10) => true)
+(fact (ff/in-bounds? p1  -1  -1 10 10) => true)
+(fact (ff/in-bounds? p1   0   0 10 10) => false)
 
-(fact (count (ff/fill (p/point -1 -1) points blocked-value-fn blocked-decider-fn)) => 77)
+(fact (count (ff/test-neighbours2 p1 negative-decider-fn -10 -10 10 10)) => 3)
+(fact (count (ff/test-neighbours2 p2 negative-decider-fn -10 -10 10 10)) => 1)
+(fact (count (ff/test-neighbours2 p3 negative-decider-fn -10 -10 10 10)) => 8)
 
-(fact (count (ff/partition points negative-value-fn negative-decider-fn)) => 97)
+(fact (count (ff/fill p1 points negative-decider-fn)) => 25)
+(fact (count (ff/fill p1 points blocked-decider-fn)) => 77)
 
-(fact (count (ff/partition points blocked-value-fn blocked-decider-fn)) => 3)
+(fact (count (ff/fill2 p1 points negative-decider-fn -5 -5 5 5)) => 25)
+(fact (count (ff/fill2 p1 points blocked-decider-fn -5 -5 5 5)) => 77)
+
+(fact (count (ff/partition points negative-decider-fn)) => 97)
+(fact (count (ff/partition points blocked-decider-fn)) => 3)
+
+(fact (count (ff/partition2 points negative-decider-fn -5 -5 5 5)) => 97)
+(fact (count (ff/partition2 points blocked-decider-fn -5 -5 5 5)) => 3)
+
+(dorun (println "after testing first partitions"))
+
+(defn test-get-neighbours
+  [size]
+  (let [more-numbers (take size (iterate inc (* -1 (/ size 2))))
+        more-points (for [a more-numbers
+                          b more-numbers]
+                      (p/point a b))
+        starttime (System/currentTimeMillis)
+        parts (count (ff/neighbours more-points p3))
+        endtime (System/currentTimeMillis)]
+    (dorun (println (str "getting neighbours " size " took " (- endtime starttime) "ms")))))
+
+(test-get-neighbours 10)
+(test-get-neighbours 20)
+(test-get-neighbours 30)
+(test-get-neighbours 50)
+(test-get-neighbours 320)
+
+(defn test-filter1-neighbours
+  [size]
+  (let [more-numbers (take size (iterate inc (* -1 (/ size 2))))
+        more-points (for [a more-numbers
+                          b more-numbers]
+                      (p/point a b))
+        starttime (System/currentTimeMillis)
+        parts (count (ff/test-neighbours p3 (ff/add-value more-points negative-value-fn) negative-decider-fn))
+        endtime (System/currentTimeMillis)]
+    (dorun (println (str "fitering neighbours old " size " took " (- endtime starttime) "ms")))))
+
+(test-filter1-neighbours 10)
+(test-filter1-neighbours 20)
+(test-filter1-neighbours 50)
+(test-filter1-neighbours 320)
+
+(defn test-filter2-neighbours
+  [size]
+  (let [starttime (System/currentTimeMillis)
+        parts (count (ff/test-neighbours2 p3 negative-decider-fn -10 -10 10 10))
+        endtime (System/currentTimeMillis)]
+    (dorun (println (str "fitering neighbours new " size " took " (- endtime starttime) "ms")))))
+
+(test-filter2-neighbours 10)
+(test-filter2-neighbours 20)
+(test-filter2-neighbours 50)
+(test-filter2-neighbours 320)
+
+(defn test-fill1
+  [size]
+  (let [x1 (* -1 (/ size 2))
+        x2 (/ size 2)
+        more-numbers (take size (iterate inc x1))
+        more-points (for [a more-numbers
+                          b more-numbers]
+                      (p/point a b))
+        starttime (System/currentTimeMillis)
+        parts (count (ff/fill (p/point x1 x1) more-points blocked-decider-fn))
+        endtime (System/currentTimeMillis)]
+    (dorun (println (str "filling 1 " size "x" size " to " parts " took " (- endtime starttime) "ms")))))
+
+(test-fill1 10)
+(test-fill1 20)
+(test-fill1 30)
+
+(defn test-fill2
+  [size]
+  (let [x1 (* -1 (/ size 2))
+        x2 (/ size 2)
+        more-numbers (take size (iterate inc x1))
+        more-points (for [a more-numbers
+                          b more-numbers]
+                      (p/point a b))
+        starttime (System/currentTimeMillis)
+        parts (count (ff/fill2 (p/point x1 x1) more-points blocked-decider-fn x1 x1 x2 x2))
+        endtime (System/currentTimeMillis)]
+    (dorun (println (str "filling 2 " size "x" size " to " parts " took " (- endtime starttime) "ms")))))
+
+(test-fill2 10)
+(test-fill2 20)
+(test-fill2 30)
+
+(defn test-partition1
+  [size]
+  (let [x1 (* -1 (/ size 2))
+        x2 (/ size 2)
+        more-numbers (take size (iterate inc x1))
+        more-points (for [a more-numbers
+                          b more-numbers]
+                      (p/point a b))
+        starttime (System/currentTimeMillis)
+        parts (count (ff/partition more-points blocked-decider-fn))
+        endtime (System/currentTimeMillis)]
+    (dorun (println (str "partitioning 1 " size "x" size " to " parts " took " (- endtime starttime) "ms")))))
+
+(test-partition1 10)
+(test-partition1 20)
+;(test-partition 30)
+
+(defn test-partition2
+  [size]
+  (let [x1 (* -1 (/ size 2))
+        x2 (/ size 2)
+        more-numbers (take size (iterate inc x1))
+        more-points (for [a more-numbers
+                          b more-numbers]
+                      (p/point a b))
+        starttime (System/currentTimeMillis)
+        parts (count (ff/partition2 more-points blocked-decider-fn x1 x1 x2 x2))
+        endtime (System/currentTimeMillis)]
+    (dorun (println (str "partitioning 2 " size "x" size " to " parts " took " (- endtime starttime) "ms")))))
+
+(test-partition2 10)
+(test-partition2 20)
+;(test-partition 30)
+
+
