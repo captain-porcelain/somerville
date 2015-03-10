48c41c2149743f1294b7b9979abc405fdf2ea7b6
Christoph Wagner <christoph.j.wagner@gmail.com>
2015-03-10 14:25:05 +0100

    testing line fill

--- test/sanakan/mathematics/fills/line_fill_test.clj
+++ test/sanakan/mathematics/fills/line_fill_test.clj
@@ -19,12 +19,29 @@
     (and (= (:x p1) 2) (= (:x p2) 2))
     (and (> (:x p1) 2) (> (:x p2) 2))))
 
+(defn complex-decider-fn
+  [p1 p2]
+  (if (and (< (:y p1) 2) (< (:y p2) 2))
+    true
+    (or
+      (and (< (:x p1) 2) (< (:x p2) 2))
+      (and (= (:x p1) 2) (= (:x p2) 2))
+      (and (> (:x p1) 2) (> (:x p2) 2)))))
+
 ;; Verify that the test functions do as they should.
 (fact (blocked-decider-fn (p/point -1 -1) (p/point -2 -2)) => true)
 (fact (blocked-decider-fn (p/point -1 -1) (p/point  2  2)) => false)
 (fact (blocked-decider-fn (p/point  2  2) (p/point  2  2)) => true)
 (fact (blocked-decider-fn (p/point  2  2) (p/point  3  3)) => false)
 (fact (blocked-decider-fn (p/point  3  3) (p/point  4  4)) => true)
+
+(fact (complex-decider-fn (p/point  0  0) (p/point  1  1)) => true)
+(fact (complex-decider-fn (p/point  0  0) (p/point  2  2)) => false)
+(fact (complex-decider-fn (p/point  0  0) (p/point  3  2)) => false)
+(fact (complex-decider-fn (p/point  0  0) (p/point  4  1)) => true)
+(fact (complex-decider-fn (p/point  2  2) (p/point  2  2)) => true)
+(fact (complex-decider-fn (p/point  2  2) (p/point  3  3)) => false)
+(fact (complex-decider-fn (p/point  3  3) (p/point  4  4)) => true)
 
 ;; Test that we can generate points for lines and columns.
 (fact (count (lf/make-line   (p/point 0 0) 10)) => 11)
@@ -82,12 +99,50 @@
 (def matched (lf/find-matching-segment (first (first lined)) (second lined) negative-decider-fn))
 (fact matched => (l/line (p/point -5 -4) (p/point -1 -4)))
 
+;; Test that growing a segment works.
 (def matched2 (lf/attach-matching-segment (list (first (first lined))) (second lined) negative-decider-fn))
 (fact (first  matched2) => (l/line (p/point -5 -4) (p/point -1 -4)))
 (fact (second matched2) => (first (first lined)))
 
+;; Test that partitioning works correctly.
 (def parts (lf/partition (p/point -5 -5) 5 5 blocked-decider-fn))
 (fact (count      parts)    => 3)
 (fact (count (nth parts 0)) => 11)
+(dorun
+  (for [p (nth parts 0)]
+    (let []
+      (fact (:x (:p1 p)) => -5)
+      (fact (:x (:p2 p)) =>  1))))
 (fact (count (nth parts 1)) => 11)
+(dorun
+  (for [p (nth parts 1)]
+    (let []
+      (fact (:x (:p1 p)) =>  2)
+      (fact (:x (:p2 p)) =>  2))))
 (fact (count (nth parts 2)) => 11)
+(dorun
+  (for [p (nth parts 2)]
+    (let []
+      (fact (:x (:p1 p)) =>  3)
+      (fact (:x (:p2 p)) =>  5))))
+
+;; Test partitioning with changing amount of segments per line
+(def parts2 (lf/partition (p/point 0 0) 5 5 complex-decider-fn))
+(fact (count      parts2)      => 3)
+(fact (count (nth parts2 0))   => 6)
+(fact (nth   (nth parts2 0) 0) => (l/line (p/point 0 5) (p/point 1 5)))
+(fact (nth   (nth parts2 0) 1) => (l/line (p/point 0 4) (p/point 1 4)))
+(fact (nth   (nth parts2 0) 2) => (l/line (p/point 0 3) (p/point 1 3)))
+(fact (nth   (nth parts2 0) 3) => (l/line (p/point 0 2) (p/point 1 2)))
+(fact (nth   (nth parts2 0) 4) => (l/line (p/point 0 1) (p/point 5 1)))
+(fact (nth   (nth parts2 0) 5) => (l/line (p/point 0 0) (p/point 5 0)))
+(fact (count (nth parts2 1))   => 4)
+(fact (nth   (nth parts2 1) 0) => (l/line (p/point 2 5) (p/point 2 5)))
+(fact (nth   (nth parts2 1) 1) => (l/line (p/point 2 4) (p/point 2 4)))
+(fact (nth   (nth parts2 1) 2) => (l/line (p/point 2 3) (p/point 2 3)))
+(fact (nth   (nth parts2 1) 3) => (l/line (p/point 2 2) (p/point 2 2)))
+(fact (count (nth parts2 2))   => 4)
+(fact (nth   (nth parts2 2) 0) => (l/line (p/point 3 5) (p/point 5 5)))
+(fact (nth   (nth parts2 2) 1) => (l/line (p/point 3 4) (p/point 5 4)))
+(fact (nth   (nth parts2 2) 2) => (l/line (p/point 3 3) (p/point 5 3)))
+(fact (nth   (nth parts2 2) 3) => (l/line (p/point 3 2) (p/point 5 2)))
