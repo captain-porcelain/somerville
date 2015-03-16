5001ff564c8ad6df9b0929544abe8b2e433f285f
Christoph Wagner <christoph.j.wagner@gmail.com>
2015-03-16 14:26:58 +0100

    try highlighting a cluster

--- src/sanakan/mathematics/visualization/fill.clj
+++ src/sanakan/mathematics/visualization/fill.clj
@@ -23,24 +23,13 @@
         cie (c/cie76 (vfn p1) (vfn p2))]
     (< cie @threshold-cie)))
 
-(defn in-line?
-  [x y line]
-  (and
-    (= y (:y (:p1 line)))
-    (<= x (:x (:p1 line)))
-    (>= x (:x (:p2 line)))))
-
-(defn in-cluster?
-  [x y cluster]
-  (if (some? (map #(in-line? x y %) cluster)) true false))
-
 (defn draw-cluster
   [cluster]
   (dorun
     (for [l cluster]
       (let [p (:p1 (first cluster))
             dc (c/rgba (.getRGB image (:x p) (:y p)))
-            dc (if (in-cluster? (quil/mouse-x) (quil/mouse-y) cluster) (c/rgba 255 255 255) dc)]
+            dc (if (lf/in-cluster? (quil/mouse-x) (quil/mouse-y) cluster) (c/rgba 255 255 255) dc)]
         (quil/stroke-float (:r dc) (:g dc) (:b dc))
         (quil/fill-float (:r dc) (:g dc) (:b dc))
         (quil/line (:x (:p1 l)) (:y (:p1 l)) (:x (:p2 l)) (:y (:p2 l)))))))
