8ac0a365094202f807ce6e5f6a9abbf6aad5e680
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2015-03-17 19:44:11 +0100

    visualize quilt

--- src/sanakan/mathematics/core.clj
+++ src/sanakan/mathematics/core.clj
@@ -1,9 +1,10 @@
 (ns sanakan.mathematics.core
   (:require [sanakan.mathematics.visualization.voronoi :as voronoi]
             [sanakan.mathematics.visualization.fill :as fill]
+            [sanakan.mathematics.visualization.quilt :as quilt]
             [quil.core :as quil])
   (:gen-class))
 
 (defn -main [& args]
-  (fill/show))
+  (quilt/show))
 
