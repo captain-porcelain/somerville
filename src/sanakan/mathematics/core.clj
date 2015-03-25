3d933c79648cb6137a3c82481986747bd478f1f5
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2015-03-25 18:47:10 +0100

    add rendering of koch curve

--- src/sanakan/mathematics/core.clj
+++ src/sanakan/mathematics/core.clj
@@ -2,9 +2,10 @@
   (:require [sanakan.mathematics.visualization.voronoi :as voronoi]
             [sanakan.mathematics.visualization.fill :as fill]
             [sanakan.mathematics.visualization.quilt :as quilt]
+            [sanakan.mathematics.visualization.l-system :as l-system]
             [quil.core :as quil])
   (:gen-class))
 
 (defn -main [& args]
-  (quilt/show))
+  (l-system/show))
 
