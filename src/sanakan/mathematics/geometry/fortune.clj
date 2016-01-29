99078edd21c00cf1f60158e86706e9e264237b0d
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2016-01-29 19:20:34 +0100

    further extensions for fortunes algorithm

--- src/sanakan/mathematics/geometry/fortune.clj
+++ src/sanakan/mathematics/geometry/fortune.clj
@@ -3,6 +3,7 @@
     [clojure.zip :as z]
     [sanakan.mathematics.geometry.commons :as c]
     [sanakan.mathematics.geometry.line :as l]
+    [sanakan.mathematics.geometry.parabola :as par]
     [sanakan.mathematics.geometry.point :as p]))
 
 ;; define a site event type.
@@ -26,12 +27,32 @@
   [points]
   (sort-events (map #(event % :site) points)))
 
+(defrecord TreeNode [point parabola left right]
+  c/Printable
+  (c/out [this i] (str (c/indent i) "Node at " (c/out point) " has "
+                       (if (nil? parabola) "no parabola yet " (c/out parabola))
+                       "\n" (c/indent (+ i 2)) (if (nil? left) "-" (c/out left)) " <-> " (if (nil? right) "-" (c/out right)) "\n"))
+  (c/out [this] (c/out this 0)))
+
+
 ;; define the data structure we need to represent a voronoi diagram.
-(defrecord Voronoi [events tree])
+(defrecord Voronoi [points events tree step]
+  c/Printable
+  (c/out [this i] (str (c/indent i) "Voronoi step " step " for the points\n"
+                       (reduce str (interpose "\n" (for [p points] (c/out p (+ i 2)))))
+                       "\n\nconsists of queued events\n"
+                       (reduce str (interpose "\n" (for [e events] (c/out e (+ i 2)))))
+                       (if (nil? tree)
+                         "\n\nand no tree"
+                         (str "\n\nand the tree\n" (c/out tree (+ i 2))))
+                       "\n\n"))
+  (c/out [this] (c/out this 0)))
 
 (defn add-parabola
   [tree point]
-  tree)
+  (if (nil? tree)
+    (TreeNode. point nil nil nil)
+    tree))
 
 (defn remove-parabola
   [tree point]
@@ -43,11 +64,13 @@
   (let [e (first (:events v))
         tree (:tree v)]
     (Voronoi.
+      (:points v)
       (rest (:events v))
       (if (= :site (:t e))
-        (add-parabola tree (:p e))
-        (remove-parabola tree (:p e))))))
+        (add-parabola tree e)
+        (remove-parabola tree e))
+      (+ 1 (:step v)))))
 
 (defn voronoi
   [points]
-  (Voronoi. (events points) []))
+  (Voronoi. points (events points) nil 0))
