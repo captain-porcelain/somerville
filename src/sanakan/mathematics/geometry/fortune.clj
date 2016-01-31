3c8405b2a78f96a71020d478b505e2d9c0240ffb
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2016-01-31 17:50:11 +0100

    add tree editing

--- src/sanakan/mathematics/geometry/fortune.clj
+++ src/sanakan/mathematics/geometry/fortune.clj
@@ -7,9 +7,9 @@
     [sanakan.mathematics.geometry.point :as p]))
 
 ;; define a site event type.
-(defrecord SiteEvent [p t]
+(defrecord SiteEvent [point type]
   c/Printable
-  (c/out [this i] (str (c/indent i) "Event " t " at " (c/out p)))
+  (c/out [this i] (str (c/indent i) "Event " type " at " (c/out point)))
   (c/out [this] (c/out this 0)))
 
 (defn event
@@ -20,20 +20,29 @@
 (defn sort-events
   "Sort events by y position."
   [events]
-  (sort-by #(:y (:p %)) events))
+  (sort-by #(:y (:point %)) events))
 
 (defn events
   "Create sorted events for a list of points."
   [points]
   (sort-events (map #(event % :site) points)))
 
-(defrecord TreeNode [point parabola left right]
+(defrecord TreeNode [event parabola left right]
   c/Printable
-  (c/out [this i] (str (c/indent i) "Node at " (c/out point) " has "
-                       (if (nil? parabola) "no parabola yet " (c/out parabola))
-                       "\n" (c/indent (+ i 2)) (if (nil? left) "-" (c/out left)) " <-> " (if (nil? right) "-" (c/out right)) "\n"))
+  (c/out [this i keepfirst]
+    (str (when keepfirst (c/indent i)) "Node for " (c/out event) " has "
+         (if (nil? parabola) "no parabola yet " (c/out parabola)) "\n"
+         (c/indent (+ i 2)) "left:  " (if (nil? left)  "-" (c/out left  (+ i 2) false)) "\n"
+         (c/indent (+ i 2)) "right: " (if (nil? right) "-" (c/out right (+ i 2) false))))
+  (c/out [this i] (c/out this i true))
   (c/out [this] (c/out this 0)))
 
+(defn treenode
+  "Create a new TreeNode"
+  ([event parabola]
+   (TreeNode. event parabola nil nil))
+  ([event parabola left right]
+   (TreeNode. event parabola left right)))
 
 ;; define the data structure we need to represent a voronoi diagram.
 (defrecord Voronoi [points events tree step]
@@ -48,11 +57,52 @@
                        "\n\n"))
   (c/out [this] (c/out this 0)))
 
+(defn leaf?
+  "Is this TreeNode a leaf?"
+  [node]
+  (and (nil? (:left node)) (nil? (:right node))))
+
+(defn make-zipper
+  "Create a zipper from the root tree node."
+  [root]
+  (z/zipper
+    #(not (leaf? %))
+    #(list (:left %) (:right %))
+    (fn [node children] (TreeNode. (:event node) (:parabola node) (first children) (second children)))
+    root))
+
+(defn find-parabola
+  "Find the parabola that is to be split in two by a site event."
+  [zipper event]
+  (if (leaf? (z/node zipper))
+    zipper
+    (if (< (:x (:point event)) (:x (:point (:event (z/node zipper)))))
+      (recur (z/down zipper) event)
+      (recur (z/right (z/down zipper)) event))))
+
+(defn create-subtree
+  [node event]
+  (treenode (:event node) nil
+              (treenode (:event node) nil
+                (treenode (:event node) nil nil nil)
+                (treenode event nil nil nil))
+              (treenode (:event node) nil nil nil)))
+
+(defn edit-tree
+  [tree event]
+  (z/root (z/edit
+                (find-parabola (make-zipper tree) event)
+                (fn [n] (create-subtree n event)))))
+
 (defn add-parabola
-  [tree point]
+  [tree event]
   (if (nil? tree)
-    (TreeNode. point nil nil nil)
-    tree))
+    (treenode event nil)
+    (if (leaf? tree)
+      (treenode (:event tree) nil
+                (treenode (:event tree) nil)
+                (treenode event nil))
+      (edit-tree tree event))))
 
 (defn remove-parabola
   [tree point]
@@ -66,7 +116,7 @@
     (Voronoi.
       (:points v)
       (rest (:events v))
-      (if (= :site (:t e))
+      (if (= :site (:type e))
         (add-parabola tree e)
         (remove-parabola tree e))
       (+ 1 (:step v)))))
--- test/sanakan/mathematics/geometry/fortune_test.clj
+++ test/sanakan/mathematics/geometry/fortune_test.clj
@@ -1,6 +1,7 @@
 (ns sanakan.mathematics.geometry.fortune-test
   (:require
     [clojure.zip :as z]
+    [clojure.pprint :as pp]
     [sanakan.mathematics.geometry.commons :as c]
     [sanakan.mathematics.geometry.fortune :as f]
     [sanakan.mathematics.geometry.point :as p]
@@ -10,7 +11,7 @@
 ;; check creation and sorting of events
 (def point1 (p/point 2 2))
 (def event1 (f/event point1 :site))
-(fact (:p event1) => point1)
+(fact (:point event1) => point1)
 
 (def point2 (p/point 1 3))
 (def point3 (p/point 1 1))
@@ -19,11 +20,36 @@
 (def events1 (f/events points1))
 
 (fact (count events1) => 3)
-(fact (:p (nth events1 0)) => point3)
-(fact (:p (nth events1 1)) => point1)
-(fact (:p (nth events1 2)) => point2)
+(fact (:point (nth events1 0)) => point3)
+(fact (:point (nth events1 1)) => point1)
+(fact (:point (nth events1 2)) => point2)
 
 (def voronoi1-step0 (f/voronoi points1))
 (dorun (println (c/out voronoi1-step0)))
 (def voronoi1-step1 (f/step voronoi1-step0))
 (dorun (println (c/out voronoi1-step1)))
+(def voronoi1-step2 (f/step voronoi1-step1))
+(dorun (println (c/out voronoi1-step2)))
+(def voronoi1-step3 (f/step voronoi1-step2))
+(dorun (println (c/out voronoi1-step3)))
+
+;(def zip (f/make-zipper (:tree voronoi1-step1)))
+;(dorun (println zip))
+;(dorun (println (f/find-parabola zip 1)))
+
+;(def c1 (f/treenode (f/event (p/point 1 1) :site) nil nil nil))
+;(def c2 (f/treenode (f/event (p/point 4 4) :site) nil nil nil))
+;(def r1 (f/treenode (f/event (p/point 2 2) :size) nil c1  c2))
+;(def zip1 (f/make-zipper r1))
+;(dorun (println (c/out (z/node (f/find-parabola zip1 (f/event (p/point 3.5 0) :site))))))
+
+;(def c2c1 (f/treenode (f/event (p/point 3 3) :site) nil nil nil))
+;(def c2c2 (f/treenode (f/event (p/point 5 5) :site) nil nil nil))
+;(def c22  (f/treenode (f/event (p/point 4 4) :site) nil c2c1 c2c2))
+;(def r2   (f/treenode (f/event (p/point 2 2) :size) nil c1  c22))
+
+;(def zip2 (f/make-zipper r2))
+;(dorun (println (c/out (z/node (f/find-parabola zip2 (f/event (p/point 3.5 0) :site))))))
+
+;(def zip3 (f/make-zipper (z/root (z/edit (-> zip1 z/down z/right) (fn [n] c22)))))
+;(dorun (println (c/out (z/node (f/find-parabola zip3 (f/event (p/point 3.5 0) :site))))))
