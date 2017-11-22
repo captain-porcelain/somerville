eb2157c4776e089389416a36997fe321396686cb
Christoph Wagner <sanakan@pinktentacle.net>
2017-11-22 09:26:53 +0100

    Update version to 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject somerville "1.0.0"
+(defproject somerville "1.0.1-SNAPSHOT"
   :description "Implementation of some mathematical functions."
   :dependencies
   [[org.clojure/clojure "1.8.0"]
