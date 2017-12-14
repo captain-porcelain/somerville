58b6fc80fda918fa3bd21bd9e9970f9e773572a6
Christoph Wagner <sanakan@pinktentacle.net>
2017-12-14 22:38:41 +0100

    Refactoring for ray casting discovery

--- src/clj/somerville/geometry/polygon.clj
+++ src/clj/somerville/geometry/polygon.clj
@@ -56,10 +56,12 @@
   "Reduce line to the parts that are inside the given polygon. If the line is not inside the polygon return nil."
   [polygon line]
   (let [intersections (intersect-segments polygon line)]
+    (dorun (map #(println (c/out %)) intersections))
     (case (count intersections)
       0 (if (and (point-inside-polygon? polygon (:p1 line)) (point-inside-polygon? polygon (:p2 line))) line nil)
       1 (l/line (first (filter #(point-inside-polygon? polygon %) (list (:p1 line) (:p2 line)))) (first intersections))
-      2 (l/line (nth intersections 0) (nth intersections 1)))))
+      2 (l/line (nth intersections 0) (nth intersections 1))
+      line)))
 
 ;;====================================================================================================================================================
 ;; Polygon based visibility checking
