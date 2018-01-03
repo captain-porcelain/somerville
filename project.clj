0b0b45d462cbc9970119fc75431d47ceacfd4f7c
Christoph Wagner <sanakan@pinktentacle.net>
2018-01-03 13:50:28 +0100

    Release version 1.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject somerville "1.0.6-SNAPSHOT"
+(defproject somerville "1.0.6"
   :description "Implementation of some mathematical functions."
   :dependencies
   [[org.clojure/clojure "1.9.0"]
