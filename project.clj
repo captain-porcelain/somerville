f10716f1bbfbe51bf2bceb76b7ab5165422ecfc5
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2018-01-03 19:23:11 +0100

    Include ClojureScript to fix breaking Holytools

--- project.clj
+++ project.clj
@@ -2,12 +2,25 @@
   :description "Implementation of some mathematical functions."
   :dependencies
   [[org.clojure/clojure "1.9.0"]
+   [org.clojure/clojurescript "1.9.946"]
    [quil "2.6.0"]
    [fs "1.3.3"]
    [com.taoensso/timbre "4.10.0"]]
+
   :plugins
-  [[lein-marginalia "0.8.0"]]
+  [[lein-cljsbuild "1.1.7"]
+   [lein-marginalia "0.8.0"]]
   :main somerville.core
   :source-paths ["src/clj" "src/cljc" "src/cljs"]
   :repl-options
-  {:timeout 120000})
+  {:timeout 120000}
+  :hooks [leiningen.cljsbuild]
+
+  :cljsbuild
+  {:builds
+   [{:source-paths ["src/cljs" "src/cljc"]
+     :compiler
+     {:output-to "resources/js/somerville.js"
+      :optimizations :whitespace
+      ;:optimizations :advanced
+      :pretty-print true}}]})
