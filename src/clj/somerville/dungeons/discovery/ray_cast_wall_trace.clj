485a64fe9af65bd21fb22bd022a89ff4b7d0a1fa
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2018-01-04 18:28:43 +0100

    Move discovery debugging code to test namespace

--- src/clj/somerville/dungeons/discovery/ray_cast_wall_trace.clj
+++ src/clj/somerville/dungeons/discovery/ray_cast_wall_trace.clj
@@ -1,7 +1,4 @@
 (ns somerville.dungeons.discovery.ray-cast-wall-trace
-  (:import
-    [java.awt Color Graphics2D Rectangle AlphaComposite Polygon BasicStroke RenderingHints]
-    [java.awt.image BufferedImage])
   (:require
     [somerville.commons :as commons]
     [somerville.image :as image]
@@ -14,95 +11,6 @@
     [taoensso.timbre :as log]))
 
 (set! *warn-on-reflection* true)
-
-;;==================================================================================================================
-;; Debugging helpers
-
-(def debug (atom false))
-(def debug-fn-1 (atom nil))
-(def debug-fn (atom nil))
-(def debug-point (atom 0))
-(def debug-step (atom 0))
-
-(defn draw-triangle
-  "Transform a triangle into a Java graphics polygon and render it."
-  [triangle ^Graphics2D graphics]
-  (let [xs (into-array Integer/TYPE (list (:x (:p1 triangle)) (:x (:p2 triangle)) (:x (:p3 triangle))))
-        ys (into-array Integer/TYPE (list (:y (:p1 triangle)) (:y (:p2 triangle)) (:y (:p3 triangle))))
-        p (Polygon. xs ys (count xs))
-        tmp (.fillPolygon graphics p)
-        tmp (.drawPolygon graphics p)]))
-
-(defn debug-draw
-  "Create an image of size width x height with transparency and paint it completely black."
-  [^String filename ^Integer width ^Integer height circle points lines active-walls last-point triangles]
-  (let [img (BufferedImage. width height BufferedImage/TYPE_INT_ARGB)
-        graphics ^Graphics2D (.createGraphics img)
-        tmp (.setRenderingHint graphics RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
-        tmp (.setPaint graphics Color/white)
-        tmp (.fill graphics (Rectangle. 0 0 width height))
-        tmp (.setPaint graphics Color/black)
-        tmp (dorun (map #(.drawLine graphics (:x (:p1 %)) (:y (:p1 %)) (:x (:p2 %)) (:y (:p2 %))) lines))
-        tmp (.setPaint graphics Color/red)
-        tmp (.drawOval graphics (- (:x (:p circle)) (:r circle)) (- (:y (:p circle)) (:r circle)) (* 2 (:r circle)) (* 2 (:r circle)))
-        tmp (.drawLine graphics (- (:x (:p circle)) 5) (- (:y (:p circle)) 5) (+ (:x (:p circle)) 5) (+ (:y (:p circle)) 5))
-        tmp (.drawLine graphics (- (:x (:p circle)) 5) (+ (:y (:p circle)) 5) (+ (:x (:p circle)) 5) (- (:y (:p circle)) 5))
-        tmp (.setPaint graphics Color/green)
-        tmp (dorun (map #(do
-                           (.drawLine graphics (- (:x %) 5) (- (:y %) 5) (+ (:x %) 5) (+ (:y %) 5))
-                           (.drawLine graphics (- (:x %) 5) (+ (:y %) 5) (+ (:x %) 5) (- (:y %) 5))) points))
-        tmp (.setPaint graphics Color/blue)
-        tmp (when-not (nil? last-point)
-              (.drawLine graphics (:x last-point) (- (:y last-point) 5) (:x last-point) (+ (:y last-point) 5))
-              (.drawLine graphics (- (:x last-point) 5) (:y last-point) (+ (:x last-point) 5) (:y last-point)))
-        tmp (.setPaint graphics Color/orange)
-        tmp (dorun (map #(.drawLine graphics (:x (:p1 %)) (:y (:p1 %)) (:x (:p2 %)) (:y (:p2 %))) active-walls))
-        tmp (.setPaint graphics Color/gray)
-        tmp (dorun (map #(draw-triangle % graphics) triangles))
-        tmp (.dispose graphics)]
-    (image/write-image filename img)))
-
-(defn make-debug-fn-1
-  [width height visualrange]
-  (fn
-    [filename point points sorted-walls active-walls last-point triangles]
-    (debug-draw filename width height (c/circle point visualrange) points sorted-walls active-walls last-point triangles)))
-
-(defn make-debug-fn-2
-  [point sorted-walls]
-  (swap! debug-point inc)
-  (reset! debug-step 0)
-  (fn
-    [filename points active-walls last-point triangles]
-    (@debug-fn-1 filename point points sorted-walls active-walls last-point triangles)))
-
-(defn debug-visible-triangles
-  [remaining point new-walls current-walls last-walls new-point new-triangles relevant-event]
-  (let [starting-walls (filter #(= (:point relevant-event) (:p1 %)) (concat current-walls last-walls new-walls))
-        ending-walls (filter #(= (:point relevant-event) (:p2 %)) (concat current-walls last-walls new-walls))
-        starting-count (count starting-walls)
-        ending-count (count ending-walls)]
-    (str
-      "========================================\nEVENTS\n"
-      (clojure.string/join "\n" (map #(gcommons/out %) (first remaining)))
-      "\n----------------------------------------\nRELEVANT: " (gcommons/out relevant-event)
-      "\n----------------------------------------\nNEW WALLS\n"
-      (clojure.string/join "\n" (map #(gcommons/out %) new-walls))
-      "\n----------------------------------------\nLAST WALLS\n"
-      (clojure.string/join "\n" (map #(gcommons/out %) last-walls))
-      "\n----------------------------------------\nCURRENT WALLS\n"
-      (clojure.string/join "\n" (map #(gcommons/out %) current-walls))
-      "\n----------------------------------------\nSTARTING, ENDING: " starting-count " - " ending-count
-      "\n----------------------------------------\nTRIANGLES\n"
-      (clojure.string/join "\n" (map #(gcommons/out %) new-triangles))
-      "\n----------------------------------------\nNEW POINT\n" (gcommons/out new-point))))
-
-(defn debug-out
-  [remaining point new-walls current-walls last-walls new-point new-triangles event]
-  (let [filename (str "/tmp/discovery-point-" @debug-point "-step-" @debug-step)
-        tmp (swap! debug-step inc)
-        tmp (spit (str filename ".log") (debug-visible-triangles remaining point new-walls current-walls last-walls new-point new-triangles event))
-        tmp (@debug-fn (str filename ".png") (map :point (first remaining)) new-walls new-point new-triangles)]))
 
 ;;==================================================================================================================
 ;; Defining the events of the discovery process. Each event represents an end or starting point of a wall.
@@ -280,8 +188,7 @@
         triangles
         (let [[new-point new-triangles new-walls] (update-triangles point last-point triangles walls last-walls (first remaining) (count (rest remaining)))
               de {:remaining remaining :point point :new-walls new-walls :walls walls :last-walls last-walls :new-point new-point :new-triangles new-triangles :relevant (relevant-event point (first remaining))}
-              tmp (when-not (nil? debugmapref) (swap! debugmapref #(assoc % :steps (conj (:steps %) de))))
-              tmp (when @debug (debug-out remaining point new-walls walls last-walls new-point new-triangles (relevant-event point (first remaining))))]
+              tmp (when-not (nil? debugmapref) (swap! debugmapref #(assoc % :steps (conj (:steps %) de))))]
           (recur
             (rest remaining)
             new-point
@@ -297,7 +204,6 @@
          sorted-walls (map #(sort-line-points % point ref-point) (filter #(not (short? %)) (relevant-walls point walls visualrange polygon-steps)))
          events (gather-events sorted-walls point ref-point)
          events (concat events (list (first events)))
-         tmp (when-not (nil? @debug-fn-1) (reset! debug-fn (make-debug-fn-2 point sorted-walls)))
          tmp (when-not (nil? debugmapref) (swap! debugmapref #(assoc % :polygon-steps polygon-steps
                                                                      :point point
                                                                      :ref-point ref-point
