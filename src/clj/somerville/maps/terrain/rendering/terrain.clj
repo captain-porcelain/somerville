60107b0e8bead1904debf0123d661713c94617e9
Christoph Wagner <sanakan@pinktentacle.net>
2018-02-25 13:25:07 +0100

    cleanup terrain rendering

--- src/clj/somerville/maps/terrain/rendering/terrain.clj
+++ src/clj/somerville/maps/terrain/rendering/terrain.clj
@@ -55,52 +55,6 @@
       ;:else          [(min b 255) (min b 255) (min b 255) 255])))
 
 
-;;=======================================================================================================================
-;; Rendering the world as height blocks
-
-(defn rect
-  [graphics a b style]
-  (when (> (:y b) (:y a))
-    (.setPaint graphics (get-color style))
-    (.fillRect graphics (:x a) (:y a) (- (:x b) (:x a)) (- (:y b) (:y a)))))
-
-(defn new-image
-  "Create a new image to hold the finished tiles."
-  [config width height]
-  (let [img (BufferedImage. width height BufferedImage/TYPE_INT_ARGB)
-        graphics ^Graphics2D (.createGraphics img)
-        tmp (.setRenderingHint graphics RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
-        tmp (.setPaint graphics (get-color (:background-color config)))
-        tmp (.fill graphics (Rectangle. 0 0 width height))]
-    [img graphics]))
-
-(def default-config
-  {:background-color      [0 0 0 255]
-   :water-color           [50 150 200 128]})
-
-(defn draw
-  [g config graphics width height]
-  (let [size (:width g)
-        water-val (* size 0.7)
-        project   (partial projection/project size width height)]
-    (dotimes [y size]
-      (dotimes [x size]
-        (let [value  (grid/get-from g x y)
-              top    (project x y value)
-              bottom (project (inc x) y 0)
-              water  (project x y water-val)
-              next-val (grid/get-from g (inc x) y)
-              style  (brightness x y (- (if (int? next-val) next-val 0) value))]
-          (rect graphics top bottom style)
-          (rect graphics water bottom (:water-color config)))))))
-
-(defn render-height-blocks
-  [g config filename width height]
-  (let [[img graphics] (new-image config width height)
-        tmp (draw g config graphics width height)
-        tmp (.dispose graphics)]
-    (image/write-image filename img)))
-
 ;;---------------------------------------------------------------------------------------------------------------
 ;; Rendering the world as triangulation
 
@@ -154,26 +108,6 @@
             t2 (polygon/from-points (list (project-fn p2) (project-fn p3) (project-fn p4)))]
         [t1 t2]))))
 
-(defn polygon-min-max
-  [p k f]
-  (apply f (map k (polygon/to-points p))))
-
-(defn pair-min-max
-  [p k f]
-  (apply f (map #(polygon-min-max % k f) p)))
-
-(defn found-max
-  [p k]
-  (apply max (map #(pair-min-max % k max) p)))
-
-(defn found-min
-  [p k]
-  (apply min (map #(pair-min-max % k min) p)))
-
-(defn scale-factor
-  [triangles width offset]
-  (/ (+ width offset) (found-max triangles :x)))
-
 (defn draw-triangles
   "Draw pairs of triangles."
   [g config graphics width height]
@@ -202,50 +136,6 @@
 
 
 
-(defn debug-render
-  "Render the grid using a triangulation."
-  [filename width height]
-  (let [config default-config
-        [img graphics] (new-image config width height)
 
-        camera (p/point 1 15 10)
-        focus  (p/point 0 0  0)
-        up     (p/point 0 1  0)
-        projector (projection/projector camera focus up width height)
-        pject (fn [v] (projection/project projector v))
-
-        p1 (p/point -90  0 -10)
-        p2 (p/point  90  0 -10)
-        p3 (p/point   0 90 -10)
-        p4 (p/point   0  0   5)
-
-        pt1 (pject p1)
-        pt2 (pject p2)
-        pt3 (pject p3)
-        pt4 (pject p4)
-
-        tmp (dorun (println (c/out p1)))
-        tmp (dorun (println (c/out pt1)))
-        tmp (dorun (println (c/out p2)))
-        tmp (dorun (println (c/out pt2)))
-        tmp (dorun (println (c/out p3)))
-        tmp (dorun (println (c/out pt3)))
-        tmp (dorun (println (c/out p4)))
-        tmp (dorun (println (c/out pt4)))
-
-        t1 (list pt1 pt2 pt3)
-        t2 (list pt1 pt2 pt4)
-        t3 (list pt1 pt3 pt4)
-        t4 (list pt2 pt3 pt4)
-        ;tmp (dorun (println (c/out (polygon/from-points ps))))
-
-        ;tmp (dorun (println (c/out (polygon/from-points pps))))
-
-        tmp (draw-polygon graphics (polygon/from-points t1) [255 30 30 255] [128 20 128 255])
-        tmp (draw-polygon graphics (polygon/from-points t2) [30 255 30 255] [128 20 128 255])
-        tmp (draw-polygon graphics (polygon/from-points t3) [30 30 255 255] [128 20 128 255])
-        tmp (draw-polygon graphics (polygon/from-points t4) [30 30 30 255] [128 20 128 255])
-
-        tmp (.dispose graphics)]
-    (image/write-image filename img)))
-
+(defn render-heights
+  [g config filename width height])
