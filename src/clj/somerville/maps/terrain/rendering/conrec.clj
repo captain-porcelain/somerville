1888cc57379f456ca44f3fd64ae77bae6ebb950c
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2018-04-11 19:56:30 +0200

    Align rendering terrain with povray and conrec

--- src/clj/somerville/maps/terrain/rendering/conrec.clj
+++ src/clj/somerville/maps/terrain/rendering/conrec.clj
@@ -230,11 +230,16 @@
     (for [i (range max-lines)]
       (+ min-value (* (inc i) step)))))
 
+(defn contour-preselected
+  "Get contours of preselected height."
+  [g hs]
+  (let [triangles (triangulation g)]
+    (map #(hash-map :height % :lines (height-lines triangles %)) hs)))
+
 (defn contour
   "Get contours of heights in given steps apart."
   [g steps]
-  (let [triangles (triangulation g)]
-    (map #(hash-map :height % :lines (height-lines triangles %)) (line-heights g 1 steps))))
+  (contour-preselected g (line-heights g 1 steps)))
 
 ;;=======================================================================================================================
 ;; Rendering the height lines
@@ -250,30 +255,37 @@
     [img graphics]))
 
 (defn render-line
-  [graphics line line-color]
+  [graphics line line-color width height]
   (.setPaint graphics (color/to-awt (apply color/rgba line-color)))
-  (.drawLine graphics (:x (:p1 line)) (:y (:p1 line)) (:x (:p2 line)) (:y (:p2 line))))
+  (.drawLine graphics (- width (:x (:p1 line))) (- height (:y (:p1 line))) (- width (:x (:p2 line))) (- height (:y (:p2 line)))))
 
 (defn draw-height-lines
-  [graphics c scale lines]
-  (dorun (map #(render-line graphics (l/scale % scale) c) lines)))
+  [graphics c scale lines width height]
+  (dorun (map #(render-line graphics (l/scale % scale) c width height) lines)))
+
+(defn get-colors
+  [config]
+  (if (nil? (:colors config))
+    (let [c1 (apply color/rgba (:line-color-1 config))
+          c2 (apply color/rgba (:line-color-2 config))]
+      (map color/to-vector (color/color-steps c1 c2 (:height-steps config))))
+    (:colors config)))
 
 (def default-config
   {:background-color      [0 0 0 255]
    :line-color-1          [128 20 128 255]
    :line-color-2          [255 255 255 255]
+   :heights               [-1000 10 20 28 32]
+   :colors                [[0 161 228 255] [204 88 3 255] [137 252 0 255] [160 160 160 255] [240 240 240 255]]
    :height-steps          4})
 
 (defn render
   "Render height lines as seen from above."
   [g config filename width height]
   (let [[img graphics] (new-image config width height)
-        contour (contour g (:height-steps config))
+        contour (if (nil? (:heights config)) (contour g (:height-steps config)) (contour-preselected g (:heights config)))
         scale (/ width (:width g))
-        c1 (apply color/rgba (:line-color-1 config))
-        c2 (apply color/rgba (:line-color-2 config))
-        colors (map color/to-vector (color/color-steps c1 c2 (:height-steps config)))
-        tmp (dorun (map #(draw-height-lines graphics %2 scale %1) (map :lines contour) colors))
+        tmp (dorun (map #(draw-height-lines graphics %2 scale %1 width height) (map :lines contour) (get-colors config)))
         tmp (.dispose graphics)]
     (image/write-image filename img)))
 
