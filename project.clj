b93a59e63be8d1c392d83d7d1b36558d81a5d14a
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2018-06-29 18:08:55 +0200

    Release 1.0.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject somerville "1.0.8-SNAPSHOT"
+(defproject somerville "1.0.8"
   :description "Implementation of some mathematical functions."
   :dependencies
   [[org.clojure/clojure "1.9.0"]
