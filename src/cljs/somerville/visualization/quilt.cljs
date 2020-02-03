722247206b585c5c077a5721f4f5dfa569aba90c
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2020-02-03 19:49:14 +0000

    Move quilt visualization to cljs

--- src/cljs/somerville/visualization/quilt.cljs
+++ src/cljs/somerville/visualization/quilt.cljs
@@ -3,10 +3,8 @@
             [somerville.geometry.point :as p]
             [somerville.geometry.line :as line]
             [somerville.geometry.voronoi :as voronoi]
-            [somerville.rendering.image :as i]
             [somerville.color.color :as c]
-            [quil.core :as quil])
-  (:gen-class))
+            [quil.core :as quil :include-macros true]))
 
 (def test-image (atom nil))
 (def partitions-all (atom nil))
@@ -14,17 +12,17 @@
 (def draw-mode (atom 0))
 (def threshold-cie (atom 10))
 (def threshold-cluster (atom 0))
-(def filename  "resources/test-image2.jpg")
-(def image (i/load-image filename))
-(def width (.getWidth image))
-(def height (.getHeight image))
+(def filename  "../test-image2.jpg")
+(def image (atom nil))
+(def width 679)
+(def height 452)
 (def alt (atom false))
 (def points (atom (list)))
 (def sites (atom nil))
 
 (defn decider-fn
   [p1 p2]
-  (let [vfn (fn [p] (c/rgba (.getRGB image (:x p) (:y p))))
+  (let [vfn (fn [p] (c/rgba (quil/get-pixel @image (:x p) (:y p))))
         cie (c/cie76 (vfn p1) (vfn p2))]
     (< cie @threshold-cie)))
 
@@ -33,30 +31,30 @@
   (dorun
     (for [l cluster]
       (let [p (:p1 (first cluster))
-            dc (c/rgba (.getRGB image (:x p) (:y p)))
+            dc (c/rgba (quil/get-pixel @image (:x p) (:y p)))
             dc (if (lf/in-cluster? (p/point (quil/mouse-x) (quil/mouse-y)) cluster) (c/rgba 255 255 255) dc)]
-        (quil/stroke-float (:r dc) (:g dc) (:b dc))
-        (quil/fill-float (:r dc) (:g dc) (:b dc))
+        (quil/stroke (:r dc) (:g dc) (:b dc))
+        (quil/fill (:r dc) (:g dc) (:b dc))
         (quil/line (:x (:p1 l)) (:y (:p1 l)) (:x (:p2 l)) (:y (:p2 l)))))))
 
 (defn draw-intersection
   [i]
-  (quil/stroke-float 0 0 255)
-  (quil/fill-float 0 0 255)
+  (quil/stroke 0 0 255)
+  (quil/fill 0 0 255)
   (quil/rect (:x (:intersection i)) (:y (:intersection i)) 4 4))
 
 (defn draw-bisector
   [bisector]
   (let [y0 (line/solve-line-at (:line bisector) 0)
         y1 (line/solve-line-at (:line bisector) width)]
-    (quil/stroke-float 0 255 0)
-    (quil/fill-float 0 255 0)
+    (quil/stroke 0 255 0)
+    (quil/fill 0 255 0)
     (quil/line 0 y0 width y1)))
 
 (defn draw-site
   [site]
-  (quil/stroke-float 255 0 0)
-  (quil/fill-float 255 0 0)
+  (quil/stroke 255 0 0)
+  (quil/fill 255 0 0)
   (quil/rect (- (:x (:point site)) 2) (- (:y (:point site)) 2) 4 4)
   ;(dorun
   ;  (for [b (:bisectors site)]
@@ -75,15 +73,15 @@
   (dorun
     (for [l (:lines cell)]
       (let []
-        (quil/stroke-float 255 255 0)
-        (quil/fill-float 255 255 0)
+        (quil/stroke 255 255 0)
+        (quil/fill 255 255 0)
         (quil/line (:x (:p1 l)) (:y (:p1 l)) (:x (:p2 l)) (:y (:p2 l))))))
   (dorun
     (when (not (nil? @highlighted))
      (for [l (:lines @highlighted)]
         (let []
-          (quil/stroke-float 255 255 255)
-          (quil/fill-float 255 255 255)
+          (quil/stroke 255 255 255)
+          (quil/fill 255 255 255)
           (quil/rect (- (:x (:point @highlighted)) 2) (- (:y (:point @highlighted)) 2) 4 4)
           (quil/line (:x (:p1 l)) (:y (:p1 l)) (:x (:p2 l)) (:y (:p2 l))))))))
 
@@ -98,9 +96,9 @@
 (defn do-partition
   []
   (let [tmp (dorun (println "partitionning ..."))
-        starttime (System/currentTimeMillis)
+        starttime (.now js/Date)
         parts1 (lf/clusters (p/point 0 0) (- width 1) (- height 1) decider-fn)
-        endtime (System/currentTimeMillis)
+        endtime (.now js/Date)
         sizes (map lf/cluster-size parts1)
         avg1 (float (/ (reduce + sizes) (count sizes)))
         tmp (dorun (println (str "... done. found " (count parts1) " partitions with avg of " avg1 " points in " (- endtime starttime) " ms.")))]
@@ -119,9 +117,9 @@
 (defn draw
   "This function is called by quil repeatedly."
   []
-  (quil/background-float 0)
-  (quil/stroke-float 0 255 0)
-  (quil/fill-float 0 255 0)
+  (quil/background 0)
+  (quil/stroke 0 255 0)
+  (quil/fill 0 255 0)
   (when (= 0 @draw-mode) (quil/image @test-image 0 0))
   (when (= 1 @draw-mode) (dorun (for [cl @partitions] (draw-cluster cl))))
   (when (= 2 @draw-mode) (dorun (for [site (:points @sites)] (draw-site site))))
@@ -134,8 +132,10 @@
   (quil/fill 226)
   (quil/frame-rate 1)
   (reset! test-image (quil/load-image filename))
-  (dopartition)
-  (dovoronoi))
+  (reset! image (quil/load-image filename))
+  ;(dopartition)
+  ;(dovoronoi)
+  )
 
 (defn mouse-pressed [])
 (defn mouse-released [])
@@ -175,9 +175,9 @@
           dm (if (= 3 dm) 0 dm)]
       (reset! draw-mode dm))))
 
-(defn show []
-  (quil/sketch
-    :title "line fill"
+(defn ^:export show []
+  (quil/defsketch quilt
+    :host "hostelement"
     :setup setup
     :draw draw
     :size [width height]
