9b4f631c5876a28fa7972079026f95a3d8532e81
Christoph Wagner-Prowald <chris@pinkbubbles.de>
2020-02-04 19:51:54 +0000

    Add basic visualization navigation

--- src/cljs/somerville/visualization/commons.cljs
+++ src/cljs/somerville/visualization/commons.cljs
@@ -5,11 +5,30 @@
     [quil.core :as quil :include-macros true]
     [reagent.core :as reagent]))
 
+(def current-view (reagent/atom "index"))
+
+(defn index
+  "Show visualizations index."
+  [props]
+  [:div
+   [:h2 "Visualization Index"]
+   [:ul
+    [:li {:on-click #(reset! current-view "terrain")} "Terrain Heightlines"]]])
+
+(defn switch
+  "Switch the shown view."
+  [props]
+  (case @current-view
+    "terrain" [terrain/terrain]
+    [index]))
+
 (defn framing
   "Show a framing around the visualizations."
   [props]
+  (log/info @current-view)
   [:div
-   [terrain/terrain]])
+   [:h1 {:on-click #(reset! current-view "index")} "Somerville"]
+   [switch]])
 
 (defn show
   "Start reagent app."
